7324301b0d42b65868f512e146197efbd5bdbc30
Rob Fielding <rob.fielding@gmail.com>
2014-03-02 00:52:36 -0500

    adding in more trig computations

--- rational.clj
+++ rational.clj
@@ -2,6 +2,15 @@
 ; Algebraic Geometry done according to Normal Wildberger's
 ;  Rational Trigonometry
 ;
+; Rational Trigonometry works over large classes of fields,
+; including prime finite fields, rationals, reals, etc.
+;
+; However, in order to retain exact definitions of perpendicularity
+; and parallelism, avoiding using the reals unnecessarily is advised.
+; If input is specified in terms of integers and rationals, then output
+; will also be integers and rationals (when expressed in terms of
+; quadrance and spread)
+;
 (use `clojure.pprint)
 
 ;
@@ -37,7 +46,7 @@
 (defn one [] 1)
 (defn zero [] 0)
 
-;TODO: need to handle distributing multiplication with addition
+; Until we handle distributive property, answer isn't fully reduced
 (defn add [a b]
   (cond
     (= a (zero)) b
@@ -45,7 +54,7 @@
     (and (number? a) (number? b)) (+ a b)
     :else [`add a b]))
 
-; Distributive property isn't fully handled
+; Until we handle distributive property, answer isn't fully reduced
 (defn mul [a b]
   (cond
     (or (= a (zero)) (= b (zero))) (zero)
@@ -72,23 +81,45 @@
 ; to only know single points or lengths, and let all other items
 ; move around to fit constraints
 ;
-;
-
+; This is a type marker
 (defn point [a b]
   [`point [`val a b]])
 
+; Lines are defined as
+;   a*x + b*y + c = 0
+;
+; And notated as a ratio:
+;
+;  < a : b : c >
+;
+; Where the 2d slope is completely determined by a and b,
+; which means that all lines with same a : b are parallel.
+; But lines that are multiples of each other are also equal, because
+; the line is defined by setting the equation equal to zero.
+; This means that a normalization rule for lines would pick a convention
+; to divide everything by b if it isn't zero, else to divide everything
+; by a.  In that case, equivalent lines are numerically equal.
+; And following that convention, the comparison can ignore c just
+; to check that the lines are parallel.
+;
 (defn line [a b c]
   [`line [`val a b c]])
 
+; Constructors for when we need to symbolically represent a
+; typed quadrance
 (defn quadrance [a]
   [`quadrance a])
 
+; Constructors for when we need to symbolically represent a
+; typed quadrance
 (defn quadranceFromLength [a]
   [`quadrance (mul a a)])
 
+; A type marker for a spread
 (defn spread [a]
   [`spread a])
 
+; Functions taking 2 point arguments use this
 (defn use2Points [pa pb f]
   (def va (get pa 1))
   (def vb (get pb 1))
@@ -100,6 +131,7 @@
   (def dy (sub y2 y))
   (f dx dy x y x2 y2))
 
+; Functions taking 2 line arguments use this
 (defn use2Lines [l1 l2 f]
   (def ln1 (get l1 1))
   (def ln2 (get l2 1))
@@ -111,6 +143,7 @@
   (def c2 (get ln2 3))
   (f a1 b1 c1 a2 b2 c2))
 
+; Functions using line and point arguments use this
 (defn useLinePoint [l1 pa f]
   (def ln1 (get l1 1))
   (def a1 (get ln1 1))
@@ -121,7 +154,7 @@
   (def y (get va 2))
   (f a1 b1 c1 x y))
  
-
+; Find the spread between 2 lines
 (defn spreadFrom2Lines [l1 l2]
   (defn _use2LinesSpread [a1 b1 c1 a2 b2 c2]
     (def n (sub (mul a1 b2) (mul a2 b1)))
@@ -132,7 +165,7 @@
     (spread (div n2 d)))
   (use2Lines l1 l2 _use2LinesSpread))
 
-   
+; Find distance squared (quadrance) between two points   
 (defn quadranceFrom2Points [pa pb]
   (defn _use2PointsForQuadrance [dx dy x y x2 y2]
     (add (mul dx dx) (mul dy dy)))
@@ -158,7 +191,8 @@
     (point (div xn d) (div yn d)))
   (use2Lines l1 l2 _use2LinesIntersection))
 
-
+; Given a line and a point, find a new line that goes through the point,
+; such that this line is parallel to the existing one.
 (defn parallelFromLinePoint [l1 pa]
   (defn _parallelLinePoint [a1 b1 c1 x y]
     (def a a1)
@@ -167,6 +201,7 @@
     (line a b c))
   (useLinePoint l1 pa _parallelLinePoint))
 
+; Given a line and a point, find a line that goes through both
 (defn altitudeFromLinePoint [l1 pa]
   (defn _altitudeLinePoint [a1 b1 c1 x y]
     (def a (mul (minusone) b1))
@@ -175,26 +210,39 @@
     (line a b c))
   (useLinePoint l1 pa _altitudeLinePoint))
 
-
+; TODO: given a reference line with a point on it and a spread,
+; compute possible points for a given quadrance along that spread
 ;
 ; Define example problems to test the APIs 
 ;
- 
+; For now, the definitions need to be nested so that
+; inner objects are actualized.  Since lines can be
+; determined from points, or points from lines, doing this
+; sorting is something that the code could do eventually.
+; But for now, the symbolic manipulation is too simplistic.
+;
+
+; Some points, and the associated lines with making a triangle of them
 (def pA (point 0 0))
 (def pB (point 4 0))
 (def pC (point 4 3))
-(def pD (point 2 8))
 (def lC (lineFrom2Points pA pB))
 (def lB (lineFrom2Points pC pA))
 (def lA (lineFrom2Points pB pC))
 
-(def findIntersection (intersectionPointFrom2Lines lA lB))
+; Some line not associated with any points
+(def lD (line 3 -1 6))
+(def lF (line 5 2 9))
+; And a point generated at their intersection
+(def pD (intersectionPointFrom2Lines lD lF))
 
-(printd findIntersection)
+; Generate a new point
+(def lE (altitudeFromLinePoint lC pD))
+
+(printd pD)
 (printd (quadranceFrom2Points pA pC))
-(printd (spreadFrom2Lines lB lC))
-(printd (altitudeFromLinePoint lC pD))
-(printd (parallelFromLinePoint lC pD))
+(printd (spreadFrom2Lines lD lC))
+(printd (parallelFromLinePoint lE pA))
 
 
 
