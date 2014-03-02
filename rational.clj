5c7d8ead83e58ff5729911abde25aee5c4d88fca
Rob Fielding <rob.fielding@gmail.com>
2014-03-02 00:15:01 -0500

    start adding basic quadrance and spread code

--- rational.clj
+++ rational.clj
@@ -18,7 +18,13 @@
 ; then promote to floats.  If you don't allow floats to
 ; pollute the result, then equality checks are exact,
 ; which is important for parallel and perpendicular checks
+; Also, if results include non-numerics, they answers can grow
+; with the complexity of the calculations used to produce it.
+; Currently, a few things like implementation of the distributive law
+; are not written which would otherwise give a more reduced result.
 ;
+; Todo: do fields need to be commutative?  If not, then there may
+; be bugs in my code based on commutativity assumptions.
 (declare add)
 (declare mul)
 (declare div)
@@ -39,6 +45,7 @@
     (and (number? a) (number? b)) (+ a b)
     :else [`add a b]))
 
+; Distributive property isn't fully handled
 (defn mul [a b]
   (cond
     (or (= a (zero)) (= b (zero))) (zero)
@@ -58,6 +65,13 @@
 
 ;
 ; Now we can start to define geometry
+;
+; For now, we have a problem with being forced to actualize points and lengths.
+; It is perfectly fine to solve triangles based on ratios such that
+; no lengths or absolute points are actually known. It is also ok
+; to only know single points or lengths, and let all other items
+; move around to fit constraints
+;
 ;
 
 (defn point [a b]
@@ -97,42 +111,70 @@
   (def c2 (get ln2 3))
   (f a1 b1 c1 a2 b2 c2))
 
-(defn use2LinesSpread [a1 b1 c1 a2 b2 c2]
-  (def n (sub (mul a1 b2) (mul a2 b1)))
-  (def n2 (mul n n))
-  (def d1 (add (mul a1 a1) (mul b1 b1)))  
-  (def d2 (add (mul a2 a2) (mul b2 b2)))
-  (def d (mul d1 d2))
-  (spread (div n2 d)))
+(defn useLinePoint [l1 pa f]
+  (def ln1 (get l1 1))
+  (def a1 (get ln1 1))
+  (def b1 (get ln1 2))
+  (def c1 (get ln1 3))
+  (def va (get pa 1))
+  (def x (get va 1))
+  (def y (get va 2))
+  (f a1 b1 c1 x y))
+ 
 
 (defn spreadFrom2Lines [l1 l2]
-  (use2Lines l1 l2 use2LinesSpread))
+  (defn _use2LinesSpread [a1 b1 c1 a2 b2 c2]
+    (def n (sub (mul a1 b2) (mul a2 b1)))
+    (def n2 (mul n n))
+    (def d1 (add (mul a1 a1) (mul b1 b1)))  
+    (def d2 (add (mul a2 a2) (mul b2 b2)))
+    (def d (mul d1 d2))
+    (spread (div n2 d)))
+  (use2Lines l1 l2 _use2LinesSpread))
 
-(defn use2PointsForQuadrance [dx dy x y x2 y2]
-  (add (mul dx dx) (mul dy dy)))
    
 (defn quadranceFrom2Points [pa pb]
-  (use2Points pa pb use2PointsForQuadrance))
+  (defn _use2PointsForQuadrance [dx dy x y x2 y2]
+    (add (mul dx dx) (mul dy dy)))
+  (use2Points pa pb _use2PointsForQuadrance))
 
-(defn use2PointsForLine [dx dy x y x2 y2]
-  (def a (mul dy (minusone)))
-  (def b dx)
-  (def c (sub (mul dy x) (mul dx y)))
-  (line a b c))
  
 ;Given 2 points, compute the line through them
 (defn lineFrom2Points [pa pb]
-  (use2Points pa pb use2PointsForLine)) 
+  (defn _use2PointsForLine [dx dy x y x2 y2]
+    (def a (mul dy (minusone)))
+    (def b dx)
+    (def c (sub (mul dy x) (mul dx y)))
+    (line a b c))
+  (use2Points pa pb _use2PointsForLine)) 
 
-(defn use2LinesIntersection [a1 b1 c1 a2 b2 c2]
-  (def xn (sub (mul b1 c2) (mul b2 c1))) 
-  (def yn (sub (mul c1 a2) (mul c2 a1)))
-  (def d (sub (mul a1 b2) (mul a2 b1)))
-  (point (div xn d) (div yn d)))
 
 ;Given 2 lines, compute the intersection
 (defn intersectionPointFrom2Lines [l1 l2]
-  (use2Lines l1 l2 use2LinesIntersection))
+  (defn _use2LinesIntersection [a1 b1 c1 a2 b2 c2]
+    (def xn (sub (mul b1 c2) (mul b2 c1))) 
+    (def yn (sub (mul c1 a2) (mul c2 a1)))
+    (def d (sub (mul a1 b2) (mul a2 b1)))
+    (point (div xn d) (div yn d)))
+  (use2Lines l1 l2 _use2LinesIntersection))
+
+
+(defn parallelFromLinePoint [l1 pa]
+  (defn _parallelLinePoint [a1 b1 c1 x y]
+    (def a a1)
+    (def b b1)
+    (def c (mul (minusone) (add (mul a1 x) (mul b1 y))))
+    (line a b c))
+  (useLinePoint l1 pa _parallelLinePoint))
+
+(defn altitudeFromLinePoint [l1 pa]
+  (defn _altitudeLinePoint [a1 b1 c1 x y]
+    (def a (mul (minusone) b1))
+    (def b a1)
+    (def c (sub (mul b1 x) (mul a1 y)))
+    (line a b c))
+  (useLinePoint l1 pa _altitudeLinePoint))
+
 
 ;
 ; Define example problems to test the APIs 
@@ -141,6 +183,7 @@
 (def pA (point 0 0))
 (def pB (point 4 0))
 (def pC (point 4 3))
+(def pD (point 2 8))
 (def lC (lineFrom2Points pA pB))
 (def lB (lineFrom2Points pC pA))
 (def lA (lineFrom2Points pB pC))
@@ -150,6 +193,8 @@
 (printd findIntersection)
 (printd (quadranceFrom2Points pA pC))
 (printd (spreadFrom2Lines lB lC))
+(printd (altitudeFromLinePoint lC pD))
+(printd (parallelFromLinePoint lC pD))
 
 
 
