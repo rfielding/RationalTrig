c6ceade6c3781a69cdc9330d1ae7cc1fe1616947
Rob Fielding <rob.fielding@gmail.com>
2014-03-01 22:32:45 -0500

    start adding basic quadrance and spread code

--- rational.clj
+++ rational.clj
@@ -15,7 +15,9 @@
 ; These functions could be replaced with implementations
 ; that define different fields.  We have a field that
 ; will start with integers, promote to rationals, and
-; then promote to floats
+; then promote to floats.  If you don't allow floats to
+; pollute the result, then equality checks are exact,
+; which is important for parallel and perpendicular checks
 ;
 (declare add)
 (declare mul)
@@ -64,6 +66,42 @@
 (defn line [a b c]
   [`line [`val a b c]])
 
+(defn quadrance [a]
+  [`quadrance a])
+
+(defn quadranceFromLength [a]
+  [`quadrance (mul a a)])
+
+(defn spread [a]
+  [`spread a])
+
+(defn spreadFrom2Lines [l1 l2]
+  (def ln1 (get l1 1))
+  (def ln2 (get l2 1))
+  (def a1 (get ln1 1))
+  (def b1 (get ln1 2))
+  (def c1 (get ln1 3))
+  (def a2 (get ln2 1))
+  (def b2 (get ln2 2))
+  (def c2 (get ln2 3))
+  (def n (sub (mul a1 b2) (mul a2 b1)))
+  (def n2 (mul n n))
+  (def d1 (add (mul a1 a1) (mul b1 b1)))  
+  (def d2 (add (mul a2 a2) (mul b2 b2)))
+  (def d (mul d1 d2))
+  (spread (div n2 d)))
+  
+(defn quadranceFrom2Points [pa pb]
+  (def va (get pa 1))
+  (def vb (get pb 1))
+  (def x (get va 1))
+  (def x2 (get vb 1))
+  (def y (get va 2))
+  (def y2 (get vb 2))
+  (def dx (sub x2 x))
+  (def dy (sub y2 y))
+  (add (mul dx dx) (mul dy dy)))
+ 
 ;Given 2 points, compute the line through them
 (defn lineFrom2Points [pa pb]
   (def va (get pa 1))
@@ -105,9 +143,11 @@
 (def lB (lineFrom2Points pC pA))
 (def lA (lineFrom2Points pB pC))
 
-(printd lC)
-(printd lB)
-(printd lA)
-(printd (intersectionPointFrom2Lines lA lB))
+(def findIntersection (intersectionPointFrom2Lines lA lB))
+
+(printd findIntersection)
+(printd (quadranceFrom2Points pA pC))
+(printd (spreadFrom2Lines lB lC))
+
 
 
